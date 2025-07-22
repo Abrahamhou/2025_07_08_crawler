c82293448ce6505e3d45cdcc9ddf138074d68d1f
Abrahamhou <abrahamhou777@gmail.com>
2025-07-22 11:53:18 +0800

    modify

--- lesson4/lesson5/lesson5_3.py
+++ lesson4/lesson5/lesson5_3.py
@@ -5,13 +5,14 @@ async def main():
     #建立一個AsyncWebCrawler的實體
     async with AsyncWebCrawler() as crawler:
         #Run the crawler on a URL
-        result = await crawler.arun(url='https://crawl4ai.com')
+        url = 'https://blockcast.it/2025/07/21/eths-most-hated-rally-could-trigger-331m-in-liquidations/'
+        result = await crawler.arun(url=url)
         print(type(result))
         print("=" * 20)        
 
         #列印取出的結果
         
-        print(result.markdown.raw_markdown[:200])
+        print(result.markdown)
         
 
 if __name__ == "__main__":
